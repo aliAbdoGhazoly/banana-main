c642c84473425ca8f3f987e6d5df64a3cd4f277e
ali elghazoly <73541691+aliAbdoGhazoly@users.noreply.github.com>
2021-03-02 22:03:49 +0200

    adding-payment

--- controllers/client/shop.js
+++ controllers/client/shop.js
@@ -960,19 +960,17 @@ exports.onSuccessPayment =  (req, res, next) => {
            const encResp = req.body.encResp;
 
            let  ccavResponse = ccav.decrypt(encResp,workingKey);
-           var strArray = ccavResponse.split("&");
-           var resObject =  {};
+           
 
-           for(var i=0; i< strArray.length; i++){
-             var tempArray = strArray[i].split("=");
-             resObject[tempArray[0]] = tempArray[1]; 
-           }
-
-            res.status(200).json({
-                encResp:encResp,
-                decResp:ccavResponse,
-                objResp:resObject
-            });
+            var pData = '';
+            pData = '<table border=1 cellspacing=2 cellpadding=2><tr><td>'	
+            pData = pData + ccavResponse.replace(/=/gi,'</td><td>')
+            pData = pData.replace(/&/gi,'</td></tr><tr><td>')
+            pData = pData + '</td></tr></table>'
+            var htmlcode = '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title>finished</title></head><body><center><font size="4" color="blue"><b>Response Page</b></font><br>'+ pData +'</center><br></body></html>';
+            res.writeHeader(200, {"Content-Type": "text/html"});
+            res.write(htmlcode);
+            res.end();
 
         // The 'output' variable is the CCAvenue Response in JSON Format
         
